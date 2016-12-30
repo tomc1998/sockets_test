d194814f877519d6df8ae99423a11d76936f6d55
Thomas Cheng <thomascheng1998@googlemail.com>
2016-12-30 17:06:34 +0000

    Added code to the server and client. Server constantly listens for new connections and pushes data through the socket upon connection. Client connects to the server, listens for a response, prints the response and exits.

--- client/src/main.c
+++ client/src/main.c
@@ -1,7 +1,108 @@
+/* 
+ * Client code. See 
+ * http://www.binarytides.com/winsock-socket-programming-tutorial/ 
+ * for details 
+ */
+
 #include <stdio.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+
+/**
+ * Log winsock error, then return the error code as an integer.
+ * @return The result of WSAGetLastError().
+ */
+int log_wsa_err() {
+  int err_num = WSAGetLastError();
+  printf("Failed. Error code %d", err_num);
+  return err_num;
+}
+
+/**
+ * Setup WSA, create a socket and bind it to the given port.
+ * @param[out] wsa Winsock API details will be put here.
+ * @param[out] sock Socket will be created as this address.
+ * @param[in] server_inet_addr The string name of the server's IP address (X.X.X.X)
+ * @param[in] port The port to bind the socket to.
+ * @return The error code. 0 for success, error otherwise.
+ */
+int setup_wsa(WSADATA* wsa, SOCKET* sock, char const* server_inet_addr, int port) {
+  struct sockaddr_in server_addr; /* The server address */
+  int connect_err; /* The error received on connecting the socket */
+
+  /* Initialise winsock */
+  /* MAKEWORD(2, 2) means to use winsock 2.2 */
+  /* &wsa will put the details of the winsock implementation in wsa. */
+  printf("Initialising winsock...\n");
+  if (WSAStartup(MAKEWORD(2, 2), wsa) != 0) {
+    return log_wsa_err();
+  }
+
+  /* Create socket */
+  /* AF_INET makes an IPv4 socket (not IPv6) */
+  /* SOCK_STREAM makes a connection based protocol (like TCP) */
+  /* IPPROTO_TCP specifies we use TCP */
+  *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  printf("Creating socket...\n");
+  if (*sock == INVALID_SOCKET) {
+    return log_wsa_err();
+  }
+
+  /* Set server address */
+
+  /* Set address to be localhost, see documentation for details */
+  InetPton(AF_INET, "127.0.0.1", &(server_addr.sin_addr.s_addr));
+  server_addr.sin_family = AF_INET;   /* IPv4 */
+  server_addr.sin_port = htons(5000); /* Port 5000 */
+
+  /* Connect to server */
+  printf("Connecting to server...\n");
+  connect_err = connect(*sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (connect_err < 0) {
+    return log_wsa_err();
+  }
+
+  return 0;
+}
+
+/**
+ * A blocking functions which will wait until the server has sent a message,
+ * then print that message.
+ * @param[in] sock The socket to accept connections on.
+ * @return The error code. 0 for success, error otherwise.
+ */
+int receive_msg(SOCKET* sock) {
+  char server_msg[1024]; /* A 1KB buffer for the server message */
+  int recv_size; /* The size of the server message */
+  recv_size = recv(*sock, server_msg, sizeof(server_msg), 0);
+  if (recv_size == SOCKET_ERROR) {
+    return log_wsa_err();
+  }
+  /* Add a NUL char at the end to indicate the end of the string. printf relies
+   * on this to know the length of the string - without this, we can get buffer
+   * overflow errors. */
+  server_msg[recv_size] = 0; 
+  printf("Server replied! \n%s\n", server_msg);
+
+  return 0;
+}
+
+#define SERVER_PORT 5000
+#define SERVER_INET_ADDR "127.0.0.1"
 
 int main(int argc, char** argv) {
-  printf("Hello, world!\n");
+  WSADATA wsa; /* Will contain winsock API details */
+  SOCKET sock; /* Will contain socket details */
+  int err; /* setup_wsa() err code */
+
+  /* Setup winsock api. SERVER_INET_ADDR and SERVER_PORT are macros. */
+  err = setup_wsa(&wsa, &sock, SERVER_INET_ADDR, SERVER_PORT); 
+  if (err) { 
+    return err;
+  }
+  printf("Connected to server.\n");
+
+  receive_msg(&sock);
   return 0;
 }
 
--- server/src/main.c
+++ server/src/main.c
@@ -1,7 +1,114 @@
+/* 
+ * Server code. See 
+ * http://www.binarytides.com/winsock-socket-programming-tutorial/ 
+ * for details 
+ */
+
 #include <stdio.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+
+/**
+ * Log winsock error, then return the error code as an integer.
+ * @return The result of WSAGetLastError().
+ */
+int log_wsa_err() {
+  int err_num = WSAGetLastError();
+  printf("Failed. Error code %d", err_num);
+  return err_num;
+}
+
+/**
+ * Setup WSA, create a socket and bind it to the given port.
+ * @param[out] wsa Winsock API details will be put here.
+ * @param[out] sock Socket will be created as this address.
+ * @param[in] port The port to bind the socket to.
+ * @return The error code. 0 for success, error otherwise.
+ */
+int setup_wsa(WSADATA* wsa, SOCKET* sock, int port) {
+  struct sockaddr_in server_addr; /* The server address */
+  int bind_err; /* The error received on binding the socket */
+
+  /* Do all initialisation. See client code for some more comments in regards to this. */
+  printf("Initialising winsock...\n");
+  if (WSAStartup(MAKEWORD(2, 2), wsa) != 0) {
+    return log_wsa_err();
+  }
+  printf("Creating socket...\n");
+  *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (*sock == INVALID_SOCKET) {
+    return log_wsa_err();
+  }
+
+  /* Set address to be INADDR_ANY (we are accepting connections, so it can be any address) */
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_family = AF_INET;   /* IPv4 */
+  server_addr.sin_port = htons(port); /* Port 5000 */
+
+  /* We need to bind the socket to a port. This means that if any connections
+   * are received on this port, they will be passed to the socket. */
+  bind_err = bind(*sock, (struct sockaddr*) &server_addr, sizeof(server_addr));
+  if (bind_err == SOCKET_ERROR) {
+    return log_wsa_err();
+  }
+
+  /* Listen for connections. Max backlog of 50. */
+  listen(*sock, 50);
+
+  return 0;
+}
+
+/**
+ * A blocking function which will wait until a client has connected to the
+ * socket, accept the connection, send a message and close the connection.
+ * @param[in] sock The socket to accept connections on.
+ * @return The error code. 0 for success, error otherwise.
+ */
+int accept_connection(SOCKET* sock) {
+  struct sockaddr_in client_addr; /* The client addr */
+  int sock_size = sizeof(struct sockaddr_in); /* The size of the client addr struct */
+  SOCKET client_sock; /* The socket of the client being accepted */
+  /* Message to send to client upon connected */
+  const char* message = 
+    "Hello Client , I have received your connection. But I have to go now, \
+bye.\n";
+
+  /* When accepting a client, we create a new socket to represent their
+   * connection to our socket. */
+  client_sock = accept(*sock, (struct sockaddr*)&client_addr, &sock_size);
+  if (client_sock == INVALID_SOCKET) {
+    return log_wsa_err();
+  }
+  printf("Client connected!\n");
+
+  //Reply to client
+  send(client_sock, message, strlen(message), 0);
+
+  return 0;
+}
+
+#define SERVER_PORT 5000
 
 int main(int argc, char** argv) {
-  printf("Hello, world!\n");
+  WSADATA wsa; /* Will contain winsock API details */
+  SOCKET sock; /* Will contain socket details */
+  int err; /* setup_wsa() and accept_connection() err code */
+
+  /* Setup winsock stuff and create a socket */
+  err = setup_wsa(&wsa, &sock, SERVER_PORT); 
+  if (err) { 
+    return err;
+  }
+  printf("Server socket bound to port %d\n", SERVER_PORT);
+
+  /* Start accepting connections to sock */
+  printf("Waiting for connections...\n");
+  while(1) {
+    err = accept_connection(&sock);
+    if (err) {
+      return err;
+    }
+  }
   return 0;
 }
 
